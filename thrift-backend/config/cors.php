9ebbda84cb7cd76d7901cb8defe78d6b3c3e9903
Tonusree Talukder Trina <85805255+tonusreetalukdertrina@users.noreply.github.com>
2026-04-29 09:28:49 +0600

    Issues Fixed

--- thrift-backend/config/cors.php
+++ thrift-backend/config/cors.php
@@ -18,9 +18,7 @@ return [
     'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
-    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
